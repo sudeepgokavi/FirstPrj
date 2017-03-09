b7e4b7db176eddc736ba045574ae5f5a8747cef9
Sudeep Gokavi <sudeepgokavi@yahoo.com>
2017-03-09 20:17:57 +0530

    Updated the controller to return list.

--- src/com/firstprj/controller/FirstRestController.java
+++ src/com/firstprj/controller/FirstRestController.java
@@ -1,5 +1,8 @@
 package com.firstprj.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +11,22 @@ import com.firstprj.beans.UserBean;
 @RestController
 public class FirstRestController {
 
-	@RequestMapping(value="/users")
+	@RequestMapping(value="/user")
 	public UserBean getUserDetails(){
 		UserBean userObj = new UserBean("Sudeep", "Gokavi", 36);
 		return userObj;
 	}
+
+	@RequestMapping(value="/users")
+	public List<UserBean> getUsers(){
+		UserBean userObj = new UserBean("Sudeep", "Gokavi", 36);
+		UserBean userObj1 = new UserBean("Sudeep", "Gokavi", 36);
+		UserBean userObj2 = new UserBean("Sudeep", "Gokavi", 36);
+		List<UserBean> userBeanList = new ArrayList<UserBean>();
+		userBeanList.add(userObj1);
+		userBeanList.add(userObj);
+		userBeanList.add(userObj2);
+		return userBeanList;
+	}
+
 }
